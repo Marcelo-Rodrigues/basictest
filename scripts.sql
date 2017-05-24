13bc58faf3c56dd2d406e0af7db340f04c69a2ca
Marcelo-Rodrigues <Marcelo_rod_@hotmail.com>
2017-05-24 00:39:45 -0300

    correção

--- scripts.sql
+++ scripts.sql
@@ -1,5 +1,10 @@
 --liquibase formatted sql
 
 --changeset criacao:1
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
-    -> species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+CREATE TABLE pet (
+name VARCHAR(20), 
+owner VARCHAR(20),
+species VARCHAR(20),
+sex CHAR(1),
+birth DATE,
+death DATE);
